f118278e6e6891430aa95fe8987f2b0df6051f39
Alexey Ageev <literzap@yandex.ru>
2023-10-24 17:07:23 +0300

    Add pagination

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DeleteView, DetailView, ListView
 from django.utils.timezone import now
@@ -20,10 +21,19 @@ def create_post_request():
     )
 
 
+'''def create_pagination(request, posts):
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)'''
+
+
 def index(request):
     template = 'blog/index.html'
-    posts = create_post_request()[:settings.POST_QUANTITY]
-    context = {'post_list': posts}
+    posts = create_post_request()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, template, context)
 
 
@@ -45,7 +55,10 @@ def category_posts(request, category_slug):
         slug=category_slug
     )
     posts = create_post_request().filter(category=category)
-    context = {'category': category, 'post_list': posts}
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'category': category, 'post_list': posts, 'page_obj': page_obj}
     return render(request, template, context)
 
 
@@ -53,6 +66,10 @@ def category_posts(request, category_slug):
 def profile(request, username):
     template = 'blog/profile.html'
     profile = get_object_or_404(User, username=username)
-    context = {'profile': profile}
+    posts = create_post_request().filter(author=username)
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'profile': profile, 'page_obj': page_obj}
 
     return render(request, template, context)
